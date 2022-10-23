df898571f8e06c2d5a340a01ae64aee9bb23f12e
zhuicode99 <enlin291814833@hotmail.com>
2022-10-23 22:54:42 +0000

    complete ex7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,7 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Enter store name:"
+store_name = gets.chomp
+new_store = Store.create(name: store_name )
+puts new_store.errors.full_messages
